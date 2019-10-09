650c96920728272d0a3726801238b7d78533cf17
sreejit G K <xmsreejith@xminds.in>
2019-10-09 17:58:36 +0530

    changes

--- student.c
+++ student.c
@@ -6,12 +6,9 @@
 struct Student
 {
     /* data */
-    char studentId[100];
-    char studentName[100];
-    int studentClass;
-    char studentDivision;
+    char studentId[100],studentName[100], studentDivision,contact[12], searchKey,searchName[300], searchQuerry;
+    int studentClass, fileleng ,i ,n;
     float lastClassMark;
-    char contact[12];
 };
 
 
@@ -23,14 +20,14 @@ void main()
 
     system("clear");
     printf("\n1.Add student Details");
-    printf("\n2.Search By Student Details");
-    printf("\n3.Class Details");
+    printf("\n2.Search By Student Name");
+    printf("\n3.Student Details List");
     printf("\n4.Modify the Student Details");
     printf("\nplease select the option Number:");
     scanf("%d",&ch);
     switch (ch)
     {
-    case 1:
+        case 1:
                 file = fopen ("student.dat", "w"); 
                 if (file == NULL) 
                 { 
@@ -68,15 +65,49 @@ void main()
 
             printf("\nstudent parent contact:%s",student.contact);
 
+            // write the files content to student structure data
             fwrite (&student, sizeof(struct Student), 1, file);
             if(fwrite != 0)  
+                //if the condition satisfy the message will be print
+
                  printf("contents to file written successfully !\n"); 
             else 
                 printf("error writing file !\n"); 
   
-    // close file 
-    fclose (file);
-        break;
+            // close file 
+            fclose (file);
+                break;
+        case 2:
+                file= fopen("student.dat","w+");
+                if(file == NULL)
+                {
+                    fprintf(stderr,"\nOops file open error please check the database exist otherwise please create one\n");
+                    exit(1);
+                }
+                i=sizeof(struct Student);
+                printf("\nLength of dataBase:%d",&i);
+                
+                break;
+                
+        case 3:
+                file = fopen ("student.dat", "r"); 
+                if (file == NULL) 
+                { 
+                    fprintf(stderr, "\nOops file open error\n"); 
+                    exit (1); 
+                } 
+                
+                // read file contents till end of file 
+                printf("\nStudents List Details");
+                printf("\n---------------------");
+                while(fread(&student, sizeof(struct Student), 1, file)) 
+                    printf ("\nId=%s Name=%s Class=%d Division=%c TotalMark=%f contact=%s\n", 
+                    student.studentId,student.studentName, student.studentClass, 
+                    student.studentDivision,student.lastClassMark,student.contact); 
+            
+                // close file 
+                fclose (file); 
+                break; 
     
     default:printf("\n please check the input");
         break;
